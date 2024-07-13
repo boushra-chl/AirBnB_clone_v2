bdfd4adfd76ca5a65bb71be6fe5b0fcca5e06ec0
boushra-chl <bchilouch1998@gmail.com>
2024-07-13 21:40:42 +0100

    23

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
+"""Fabfile to distribute an archive to a web server"""
+
+
 import os.path
 from fabric.api import env
 from fabric.api import put
 from fabric.api import run
+
 
 env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server"""
-
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
