0beede9d5d2c92a8f4424001a2092d8a44d4b301
boushra-chl <bchilouch1998@gmail.com>
2024-07-14 13:53:10 +0100

    25

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,6 +20,7 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
+
     if os.path.isfile(archive_path) is False:
         return False
     file = archive_path.split("/")[-1]
